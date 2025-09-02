9388695feea1d89105b3a7716ba67c31f5b05d75
Rohan Lokesh <158269580+rohanlokesh15@users.noreply.github.com>
2025-09-02 23:02:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -225,4 +225,5 @@ def upload():
     return jsonify({"total_resumes": len(results), "results": results})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
